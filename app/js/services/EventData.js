41d10a43e4d7cc7eb4565715b1654d8684cb2571
Khotso Mokhethi <Mokhetkc@hotmail.com>
2020-12-12 17:55:49 +0200

    Services
    We completed the service module and we learned about angulars built in services and how we can create our own.

--- app/js/services/EventData.js
+++ app/js/services/EventData.js
@@ -1,15 +1,12 @@
 eventsApp.factory('eventData', function($resource) {
+    var resource = $resource('/data/event/:id', {id:'@id'}, {"getAll": {method: "GET", isArray: true, params: {something: "foo"}}});
     return {
-      getEvent: function() {
-        return $resource('/data/event/:id',{id:'@id'}).get({id:1});
-        // getEvent: function(successcb) {
-        //     $http({method: 'GET', url: '/data/event/1'}).
-        //         success(function(data, status, headers, config) {
-        //             successcb(data)
-        //         }).
-        //         error(function(data, status, headers, config) {
-        //             $log.warn(data, status, headers(), config);
-        //         });
-         } 
+        getEvent: function() {
+            return resource.get({id:1});
+        },
+        save: function(event) {
+            event.id = 999;
+            return resource.save(event);
+        }
     };
 });
